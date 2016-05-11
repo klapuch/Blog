0ae6f6c2f4365a48950d530a81ef5616158f2c1a
Dominik Klapuch <klapuchdominik@gmail.com>
2016-05-11 22:09:51 +0200

    config

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -8,12 +8,13 @@ $configurator->enableDebugger(__DIR__ . '/../log');
 
 $configurator->setTempDirectory(__DIR__ . '/../temp');
 
-/*$configurator->createRobotLoader()
-	->addDirectory(__DIR__)
-	->register();*/
-
-$configurator->addConfig(__DIR__ . '/config/config.neon');
-$configurator->addConfig(__DIR__ . '/config/config.local.neon');
+$configurator->addConfig(
+    __DIR__ . '/config/config.neon',
+    Nette\Configurator::AUTO
+);
+$configurator->addConfig(
+    __DIR__ . '/config/config.local.neon',
+    Nette\Configurator::AUTO
+);
 
 return $configurator->createContainer();
-
