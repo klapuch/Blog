8b1b855433a9bf57d2936673d93d4c2acf6a28c0
Dominik Klapuch <klapuchdominik@gmail.com>
2016-05-14 18:32:43 +0200

    updated tests

--- tests/runwin.bat
+++ tests/runwin.bat
@@ -1,2 +1,2 @@
 @echo off
-.\..\vendor\bin\tester -j 8 -o console -s -c .\php.ini -p php .\
+php .\..\www\index.php orm:generate-proxies && .\..\vendor\bin\tester -j 8 -o console -s -c .\php.ini -p php .\
