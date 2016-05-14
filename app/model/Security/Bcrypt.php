bbcffa97fb5cbbb1c829a474615ac031de77d07c
Dominik Klapuch <klapuchdominik@gmail.com>
2016-05-14 09:54:21 +0200

    Added cipher with bcrypt algo

---
+++ app/model/Security/Bcrypt.php
@@ -0,0 +1,25 @@
+<?php
+namespace Blog\Model\Security;
+
+use Nette\Security;
+
+final class Bcrypt implements Cipher {
+    const COST = 12;
+    private $password;
+
+    public function __construct(Security\Passwords $password) {
+        $this->password = $password;
+    }
+
+    public function encrypt(string $plain): string {
+        return $this->password->hash($plain, ['cost' => self::COST]);
+    }
+
+    public function decrypt(string $plain, string $hash): bool {
+        return $this->password->verify($plain, $hash);
+    }
+
+    public function deprecated(string $hash): bool {
+        return $this->password->needsRehash($hash, ['cost' => self::COST]);
+    }
+}
---
+++ app/model/Security/Cipher.php
@@ -0,0 +1,8 @@
+<?php
+namespace Blog\Model\Security;
+
+interface Cipher {
+    public function encrypt(string $plain): string;
+    public function decrypt(string $plain, string $hash): bool;
+    public function deprecated(string $hash): bool;
+}
