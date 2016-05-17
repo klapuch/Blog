12b656c21ceeabf127911c97ade91dcac1636f72
Dominik Klapuch <klapuchdominik@gmail.com>
2016-05-17 22:37:09 +0200

    fixed config

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -5,16 +5,11 @@ require __DIR__ . '/../vendor/autoload.php';
 $configurator = new Nette\Configurator;
 $configurator->setDebugMode(true);
 $configurator->enableDebugger(__DIR__ . '/../log');
-
 $configurator->setTempDirectory(__DIR__ . '/../temp');
 
 $configurator->addConfig(
     __DIR__ . '/config/config.neon',
-    Nette\Configurator::AUTO
-);
-$configurator->addConfig(
-    __DIR__ . '/config/config.local.neon',
-    Nette\Configurator::AUTO
+    Nette\Configurator::NONE
 );
 
 return $configurator->createContainer();
