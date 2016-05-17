64d38c2fba57a8ce41fe7b210a3a6b96afa3adbe
Dominik Klapuch <klapuchdominik@gmail.com>
2016-05-17 14:24:17 +0200

    CachedArticles

---
+++ app/model/CachedArticles.php
@@ -0,0 +1,37 @@
+<?php
+namespace Facedown\Model;
+
+use Nette\Caching;
+
+final class CachedArticles implements Articles {
+    private $cache;
+    private $origin;
+
+    public function __construct(Caching\IStorage $cache, Articles $origin) {
+        $this->cache = $cache;
+        $this->origin = $origin;
+    }
+
+    public function iterate(): array {
+        return $this->read(__FUNCTION__);
+    }
+
+    public function publish(string $title, string $content): Article {
+        return $this->origin->publish($title, $content);
+    }
+
+    public function article(int $id): Article {
+        return $this->read(__FUNCTION__, $id);
+    }
+
+    public function count(): int {
+        return $this->read(__FUNCTION__);
+    }
+
+    private function read(string $method, ...$args) {
+        $key = __CLASS__ . '::' . $method;
+        if($this->cache->read($key) === null)
+            $this->cache->write($key, $this->origin->$method(...$args), []);
+        return $this->cache->read($key);
+    }
+}
