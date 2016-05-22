22ad6afd011f4b39308f4d048bf1d896089d1350
Facedown <klapuchdominik@gmail.com>
2016-05-22 12:42:59 +0200

    added php doc

--- app/Model/Security/Cipher.php
+++ app/Model/Security/Cipher.php
@@ -3,7 +3,23 @@ declare(strict_types = 1);
 namespace Facedown\Model\Security;
 
 interface Cipher {
+    /**
+     * @param string $plain
+     * @return string
+     */
     public function encrypt(string $plain): string;
+
+    /**
+     * @param string $plain
+     * @param string $hash
+     * @return bool
+     */
     public function decrypt(string $plain, string $hash): bool;
+
+    /**
+     * Is the cipher deprecated and need to be encrypted again?
+     * @param string $hash
+     * @return bool
+     */
     public function deprecated(string $hash): bool;
 }
