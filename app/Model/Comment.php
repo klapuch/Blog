756f7ab9110df2d5965f26489dec3c1d3c22bd08
klapuch <klapuchdominik@gmail.com>
2016-05-26 20:02:07 +0200

    Added doc comments to interface

--- app/Model/Comment.php
+++ app/Model/Comment.php
@@ -3,10 +3,40 @@ declare(strict_types = 1);
 namespace Facedown\Model;
 
 interface Comment {
+    /**
+     * ID of the comment
+     * @return int
+     */
     public function id(): int;
+
+    /**
+     * Date of the posting
+     * @return \DateTimeInterface
+     */
     public function date(): \DateTimeInterface;
+
+    /**
+     * Content of the comment
+     * @return string
+     */
     public function content(): string;
+
+    /**
+     * Author of the posted comment
+     * @return string
+     */
     public function author(): string;
+
+    /**
+     * Is the comment visible?
+     * @return bool
+     */
     public function visible(): bool;
+
+    /**
+     * Erase the message
+     * @throws \LogicException
+     * @return void
+     */
     public function erase();
 }
