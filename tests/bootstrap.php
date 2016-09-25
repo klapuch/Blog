e08e4d7166991a11060b2037d9ef578aa26b317c
Dominik Klapuch <klapuchdominik@gmail.com>
2016-09-25 17:52:57 +0200

    Travis

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -10,15 +10,9 @@ Testbench\Bootstrap::setup(
     function (Nette\Configurator $configuration) {
         $configuration->addParameters(['testDir' => __DIR__]);
         $configuration->addConfig(__DIR__ . '/test.neon');
+        $configuration->createRobotLoader()
+            ->addDirectory(__DIR__ . '/TestCase')
+            ->register();
         $configuration->addConfig(__DIR__ . '/testbench.neon');
     }
 );
-
-$configuration = new Nette\Configurator;
-$configuration->setDebugMode(false);
-$configuration->setTempDirectory(__DIR__ . '/temp');
-$configuration->createRobotLoader()
-    ->addDirectory(__DIR__ . '/TestCase')
-    ->register();
-$configuration->addConfig(__DIR__ . '/test.neon');
-return $configuration->createContainer();
