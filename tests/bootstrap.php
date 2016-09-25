63f69824bf4b1707086244ce6f02af7ce59e5b87
Dominik Klapuch <klapuchdominik@gmail.com>
2016-09-25 18:09:07 +0200

    Travis

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,7 +6,7 @@ require __DIR__ . '/../vendor/autoload.php';
 date_default_timezone_set('Europe/Prague');
 
 Testbench\Bootstrap::setup(
-    __DIR__ . '/_temp',
+    __DIR__ . '/temp',
     function (Nette\Configurator $configuration) {
         $configuration->addParameters(['testDir' => __DIR__]);
         $configuration->addConfig(__DIR__ . '/test.neon');
